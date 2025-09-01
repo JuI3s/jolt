6db863e15701fe46c8ea9b24085dfaf3a84976d1
julius <juliuszh@stanford.edu>
2025-08-31 23:21:31 -0400

    General cube root finding algo.

--- jolt-core/src/field/cbrt.rs
+++ jolt-core/src/field/cbrt.rs
@@ -1,26 +1,19 @@
-use core::array::from_fn;
 use std::str::FromStr;
 
-use ark_bn254::Fr;
-use num::BigUint;
+use num::{BigUint, One};
 
-use crate::field::JoltField;
+use crate::{field::JoltField, utils};
 
-// s = 2
-pub const FR_THREE_ADICITY: usize = 2;
-// t = 2432026985759919469138489527250808343172040488935114927077578242952867610624
-pub const FR_TRACE_OF_MODULUS_MINUS_ONE: &[u64] = &[
-    243, 202, 698, 575, 991, 946, 913, 848, 952, 725, 080, 834, 317, 204, 048, 893, 511, 492, 707,
-    757, 824, 295, 286, 761, 062, 4,
-];
+fn modulus() -> BigUint {
+    BigUint::from_str(
+        "21888242871839275222246405745257275088548364400416034343698204186575808495617",
+    )
+    .unwrap()
+}
 
-// (t - 1) / 3
-// 810675661919973156379496509083602781057346829645038309025859414317622536874
-pub const FR_TRACE_MINUS_ONE_DIV_BY_THREE: bool = true;
-pub const FR_TRACE_PLUS_OR_MINUS_ONE_DIV_THREE: &[u64] = &[
-    810, 675, 661, 919, 973, 156, 379, 496, 509, 083, 602, 781, 057, 346, 829, 645, 038, 309, 025,
-    859, 414, 317, 622, 536, 874,
-];
+fn generator() -> BigUint {
+    BigUint::from_str("5").unwrap()
+}
 
 pub enum CbrtPrecomputation<F: JoltField> {
     TonelliShanks {
@@ -36,6 +29,46 @@ fn to_le_limb_first(arr: &[u64]) -> Vec<u64> {
     arr.iter().rev().copied().collect::<Vec<_>>()
 }
 
+fn compute_two_adic_trace(modulus: &BigUint) -> (BigUint, usize) {
+    // modulus - 1 = 2^s * t
+    let mut trace = modulus - BigUint::from_str("1").unwrap();
+    let mut s = 0;
+    while !trace.bit(0) {
+        trace >>= 1u8;
+        s += 1;
+    }
+    (trace, s)
+}
+
+fn compute_three_adic_trace(modulus: &BigUint) -> (BigUint, usize) {
+    let mut trace = modulus - BigUint::from_str("1").unwrap();
+    let mut s = 0;
+    let three = BigUint::from_str("3").unwrap();
+    let zero = BigUint::from_str("0").unwrap();
+    while &trace % &three == zero {
+        trace /= &three;
+        s += 1;
+    }
+    (trace, s)
+}
+
+fn three_adic_trace_plus_or_minus_one_div_three(trace: &BigUint) -> (BigUint, bool) {
+    let three = BigUint::from_str("3").unwrap();
+    let one = BigUint::from_str("1").unwrap();
+
+    assert_ne!(trace % &three, BigUint::from_str("0").unwrap());
+
+    if trace % &three == one {
+        ((trace - &one) / three, true)
+    } else {
+        ((trace + &one) / three, false)
+    }
+}
+
+fn compute_adic_root_of_unity(trace: &BigUint, generator: &BigUint, modulus: &BigUint) -> BigUint {
+    generator.modpow(&trace, modulus)
+}
+
 // Compute the cube root of a cube in Fr using Tonelli-Shanks.
 /// p4 section 3.1 from https://eprint.iacr.org/2009/457.pdf
 pub fn cube_root_tonelli_shanks<F: ark_ff::Field>(
@@ -49,7 +82,7 @@ pub fn cube_root_tonelli_shanks<F: ark_ff::Field>(
     // Step 1 is preprocessing
 
     // Step 2
-    let mut c = cubic_nonresidue_to_trace.inverse().unwrap();
+    let mut c = cubic_nonresidue_to_trace.clone();
     let mut r = a.pow(trace_of_modulus_minus_one);
 
     // Step 3 Computation of the cube root of (a^t)^-1
@@ -57,9 +90,9 @@ pub fn cube_root_tonelli_shanks<F: ark_ff::Field>(
     let exp = [3_u64.pow(three_adicity - 1)];
 
     let c_prime = c.pow(&exp);
+    c = c.inverse().unwrap();
 
     for i in 1..three_adicity {
-        // TODO: check this to_digit()
         let exp = [3_u64.pow(three_adicity - i - 1)];
 
         let d = r.pow(exp);
@@ -138,7 +171,10 @@ pub fn sqrt<F: ark_ff::Field>(
     } else {
         // Consistency check that if no square root is found,
         // it is because none exists.
-        debug_assert!(!matches!(elem.legendre(), ark_ff::LegendreSymbol::QuadraticResidue));
+        debug_assert!(!matches!(
+            elem.legendre(),
+            ark_ff::LegendreSymbol::QuadraticResidue
+        ));
         None
     }
 }
@@ -149,6 +185,7 @@ mod tests {
 
     use ark_bn254::Fr;
     use ark_ff::Field;
+    use ark_std::test_rng;
     use num::BigUint;
 
     use super::*;
@@ -164,15 +201,6 @@ mod tests {
         }
         x
     }
-
-    fn find_quadratic_nonresidue<F: Field>(char_minus_1_div_two: &[u64]) -> F {
-        let mut x = F::from(2_u64);
-        loop {
-            if x.pow(char_minus_1_div_two) != F::one() {
-                break;
-            }
-    }
-
     #[test]
     fn test_impl_flatten_u64_arr_to_digits() {
         let arr = [2, 0];
@@ -184,38 +212,77 @@ mod tests {
     }
 
     #[test]
-    fn test_cube_root_finding() {
-        // (21888242871839275222246405745257275088548364400416034343698204186575808495617 - 1) // 3
+    fn test_sqrt_finding() {
+        // This is the artworks sqrt implementation.
+        let modulus = modulus();
+        let generator = generator();
+        let (trace, two_adicity) = compute_two_adic_trace(&modulus);
+        let two_adic_root_of_unity =
+            compute_adic_root_of_unity(&trace, &generator, &modulus).to_string();
+        let two_adic_root_of_unity = Fr::from_str(&two_adic_root_of_unity).unwrap();
 
-        let char_minus_1_div_three = BigUint::from_str(
-            "7296080957279758407415468581752425029516121466805344781232734728858602831872",
-        )
-        .unwrap()
-        .to_u64_digits();
-
-        // Should be 3
-        let nonresidue = find_cubic_nonresidue::<Fr>(&char_minus_1_div_three);
-        let nonresidue_to_trace = nonresidue.pow(&FR_TRACE_OF_MODULUS_MINUS_ONE);
-
-        let mut cube = Fr::from(5_u64);
-        cube = cube * cube * cube;
-
-        let mut cbrt = cube_root_tonelli_shanks(
-            cube,
-            FR_THREE_ADICITY as u32,
-            &nonresidue_to_trace,
-            &to_le_limb_first(FR_TRACE_OF_MODULUS_MINUS_ONE),
-            &to_le_limb_first(FR_TRACE_PLUS_OR_MINUS_ONE_DIV_THREE),
-            FR_TRACE_MINUS_ONE_DIV_BY_THREE,
+        // test two adic root of unity is not a quadratic residue
+        let modulus_minus_one_div_two = (modulus - BigUint::from(1_u64)) / BigUint::from(2_u64);
+        assert_ne!(
+            two_adic_root_of_unity.pow(modulus_minus_one_div_two.to_u64_digits()),
+            Fr::one()
         );
-        cbrt = cbrt * cbrt * cbrt;
 
-        assert_eq!(cube, cbrt);
+        let mut square = Fr::from(2_u64);
+        square = square * square;
+        let sqrt = sqrt(
+            &square,
+            &two_adicity,
+            &two_adic_root_of_unity,
+            &(trace / BigUint::from(2_u64)).to_u64_digits(),
+        )
+        .unwrap();
+        assert_eq!(square, sqrt * sqrt);
     }
 
-    fn test_sqrt_finding() {
+    #[test]
+    fn test_cube_root_finding() {
+        let modulus = modulus();
+        let generator = generator();
+        let (trace, three_adicity) = compute_three_adic_trace(&modulus);
+        assert_eq!(three_adicity, 2);
 
+        let three_adic_root_of_unity =
+            compute_adic_root_of_unity(&trace, &generator, &modulus).to_string();
+        let three_adic_root_of_unity = Fr::from_str(&three_adic_root_of_unity).unwrap();
 
-        todo!()
+        assert_eq!(
+            Fr::from(generator.clone()).pow(trace.to_u64_digits()),
+            three_adic_root_of_unity
+        );
+        {
+            let modulus_minus_one_div_three =
+                (modulus - BigUint::from(1_u64)) / BigUint::from(3_u64);
+            assert_ne!(
+                Fr::from(generator.clone()).pow(modulus_minus_one_div_three.to_u64_digits()),
+                Fr::one()
+            );
+        }
+
+        let (trace_plus_or_minus_one_div_three, trace_minus_one_div_by_three) =
+            three_adic_trace_plus_or_minus_one_div_three(&trace);
+
+        let mut rng = test_rng();
+        let num_tests = 5;
+        for _ in 0..num_tests {
+            let mut cube = Fr::random(&mut rng);
+            cube = cube * cube * cube;
+
+            let cbrt = cube_root_tonelli_shanks(
+                cube,
+                three_adicity as u32,
+                &three_adic_root_of_unity,
+                &trace.to_u64_digits(),
+                &trace_plus_or_minus_one_div_three.to_u64_digits(),
+                trace_minus_one_div_by_three,
+            );
+
+            assert_eq!(cube, cbrt * cbrt * cbrt);
+        }
     }
 }
